1dcc3e10f5f8a56a6026f61603b92606ea712553
Leonid Stepanov <47628046+leonid-alt@users.noreply.github.com>
2025-02-23 09:13:21 +0700

    Add files via upload
    new dir

---
+++ config.py
@@ -0,0 +1,5 @@
+TOKEN='7609058050:AAFhwtc8ZX0hOii4e2RrmIOtsDA09rJyrik'
+DB_URL='sqlite+aiosqlite:///db.sqlite3'
+#DB_URL='postgresql+asyncpg://user:password@host:port/dbname[?key=value&key=value...]'
+AI_TOKEN = 'g47QWlHs5f8b9yajttoANEBcpvDhFAi3'
+# Внимание! Добавьте config.py в файл .gitignore!
